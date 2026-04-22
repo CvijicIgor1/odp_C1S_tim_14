b1c4e86dfde828f0c8116d6080b2d066aa57bf13
didi40904 <116975925+didi40904@users.noreply.github.com>
2026-04-22 18:37:51 +0200

    TeamRepo FindById
    -FindById implementirama po paraleli na  primer projekta (Almond).
    
    -Fukcija za mapiranje takodje dodata, samo sto za ipdatedAt stavlja UNDEFINED umesto NULL, jer je tako definisan pre.

---
+++ server/src/Database/repositories/teams/TeamRepository.ts
@@ -0,0 +1,71 @@
+import { ITeamRepository } from '../../../Domain/repositories/teams/ITeamRepository';
+import { DbManager } from "../../connection/DbConnectionPool";
+import { ILoggerService } from '../../../Domain/services/logger/ILoggerService';
+import { AddMemberDto } from '../../../Domain/DTOs/teams/AddMemberDto';
+import { CreateTeamDto } from '../../../Domain/DTOs/teams/CreateTeamDto';
+import { UpdateMemberRoleDto } from '../../../Domain/DTOs/teams/UpdateMemberRoleDto';
+import { UpdateTeamDto } from '../../../Domain/DTOs/teams/UpdateTeamDto';
+import { Team } from '../../../Domain/models/Team';
+import { TeamMember } from '../../../Domain/models/TeamMember';
+import { RowDataPacket, ResultSetHeader } from "mysql2";
+
+export class TeamRepository implements ITeamRepository {
+    public constructor(
+        private readonly db: DbManager,
+        private readonly logger: ILoggerService
+    ) { }
+
+    private map(r: RowDataPacket): Team {
+    return new Team(
+      r.id,
+      r.name,
+      r.description ?? null,
+      r.avatar,
+      new Date(r.createdAt),
+      r.updatedAt ? new Date(r.updatedAt) : undefined,
+    );
+    }
+
+    findAll(userId: number): Promise<Team[]> {
+        throw new Error('Method not implemented.');
+    }
+    async findById(teamId: number): Promise<Team | null> {
+        const res = await this.db.getReadConnection();
+        if (!res) return new Team();
+
+        try {
+            const [rows] = await res.conn.execute<RowDataPacket[]>(
+                `SELECT * FROM teams WHERE id = ?`,
+                [teamId]
+            );
+
+            return rows.length > 0 ? this.map(rows[0]) : new Team();
+        } catch (err) {
+            this.logger.error("TeamRepository", "findById failed", err);
+            return new Team();
+        } finally {
+            res.conn.release();
+        }
+    }
+    create(dto: CreateTeamDto, ownerId: number): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+    update(teamId: number, dto: UpdateTeamDto): Promise<Team | null> {
+        throw new Error('Method not implemented.');
+    }
+    delete(teamId: number): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+    getMembers(teamId: number): Promise<TeamMember[]> {
+        throw new Error('Method not implemented.');
+    }
+    addMember(teamId: number, dto: AddMemberDto): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+    removeMember(teamId: number, memberId: number): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+    updateMemberRole(teamId: number, memberId: number, dto: UpdateMemberRoleDto): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+}
